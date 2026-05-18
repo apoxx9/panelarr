97ea138f13f6493cae9bf1e9095b9dba1dde08e0
Lorenzo Nunez-Estevez <lorenzonunez-estevez@Lorenzos-Air.localdomain>
2026-05-18 13:30:08 +0200

    Fix wizard completion: redirect to /login instead of / to avoid 401

--- frontend/src/SetupWizard/SetupWizard.js
+++ frontend/src/SetupWizard/SetupWizard.js
@@ -117,12 +117,12 @@ class SetupWizard extends Component {
     });
 
     request.done(() => {
-      window.location.href = getPathWithUrlBase('/');
+      window.location.href = getPathWithUrlBase('/login');
     });
 
     request.fail(() => {
       // Even if the call fails, redirect so user isn't stuck
-      window.location.href = getPathWithUrlBase('/');
+      window.location.href = getPathWithUrlBase('/login');
     });
   };
 
